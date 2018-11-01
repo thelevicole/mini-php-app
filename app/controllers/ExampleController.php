ef331875c0e2bd5a73787d0114751650a47aa26e
Levi Cole <31482349+thelevicole@users.noreply.github.com>
2018-11-01 22:00:04 +0000

    Adding no .env file fallback in example

--- app/controllers/ExampleController.php
+++ app/controllers/ExampleController.php
@@ -6,7 +6,7 @@ class ExampleController {
 
 	public function index() {
 
-		$app_name = getenv('APP_NAME');
+		$app_name = getenv('APP_NAME') ?: 'No .env file';
 
 		return view('example', [
 			'title' => $app_name
