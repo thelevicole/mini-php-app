b92350d9ca1f75efbd2d2edebe97ae7f3252d7e4
Levi Cole <31482349+thelevicole@users.noreply.github.com>
2018-11-01 21:58:00 +0000

    Tidying bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -3,41 +3,45 @@
 
 	/* Definitions
 	-------------------------------------------------------- */
-	if (!defined('ROOT_DIR') || !ROOT_DIR) define('ROOT_DIR', __DIR__);
-	if (!defined('PUBLIC_DIR') || !PUBLIC_DIR) define('PUBLIC_DIR', ROOT_DIR.'/public');
-	if (!defined('APP_DIR') || !APP_DIR) define('APP_DIR', ROOT_DIR.'/app');
-	if (!defined('VIEW_DIR') || !VIEW_DIR) define('VIEW_DIR', APP_DIR.'/views');
+	foreach ([
+		'ROOT_DIR'		=> __DIR__,
+		'PUBLIC_DIR'	=> __DIR__.'/public',
+		'APP_DIR'		=> __DIR__.'/app',
+		'VIEW_DIR'		=> __DIR__.'/app/views'
+	] as $key => $value) { define($key, $value); }
 
 	/* Load enviroment variables
 	-------------------------------------------------------- */
-	$dotenv = new Dotenv\Dotenv( __DIR__ );
-	$dotenv->load();
+	if ( is_readable(ROOT_DIR.'/.env') ) {
+		$dotenv = new Dotenv\Dotenv( ROOT_DIR );
+		$dotenv->load();
+	}
 
 	/* Include user functions
 	-------------------------------------------------------- */
-	$functions_files = __DIR__.'/app/functions.php';
-	if (is_readable($functions_files)) {
+	$functions_files = ROOT_DIR.'/app/functions.php';
+	if ( is_readable($functions_files) ) {
 		require_once $functions_files;
 	}
 
 	/* Router
 	-------------------------------------------------------- */
 	$router = new AltoRouter();
-	$routes_files = __DIR__.'/app/routes.php';
-	if (is_readable($routes_files)) {
+	$routes_files = ROOT_DIR.'/app/routes.php';
+	if ( is_readable($routes_files) ) {
 		require_once $routes_files;
 	}
 
 	$match = $router->match();
 
 	// If no match throw 404
-	if (!$match) {
+	if ( !$match ) {
 		header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
 		exit;
 	}
 
 	// Else check if target is a function
-	else if (is_callable($match['target'])) {
+	else if ( is_callable($match['target']) ) {
 		call_user_func_array($match['target'], $match['params']);
 	}
 
@@ -46,7 +50,7 @@
 
 		list($controller, $action) = explode('@', $match['target']);
 
-		if (is_callable([$controller, $action])) {
+		if ( is_callable([$controller, $action]) ) {
 			call_user_func_array([$controller, $action], $match['params']);
 		} else {
 			// Routes are wrong.
